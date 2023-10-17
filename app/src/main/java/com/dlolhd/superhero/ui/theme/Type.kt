13df62b95435835b603f70d0fc770d3d4e293bf2
balay <dlolhd@gmail.com>
2023-10-17 11:37:08 +0200

    TextStyle on Typography.

--- app/src/main/java/com/dlolhd/superhero/ui/theme/Type.kt
+++ app/src/main/java/com/dlolhd/superhero/ui/theme/Type.kt
@@ -2,20 +2,31 @@ package com.dlolhd.superhero.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dlolhd.superhero.R
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_semicondensed_regular),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
+val CabinCondensed = FontFamily(
+    Font(R.font.cabin_condensed_regular),
+    Font(R.font.cabin_condensed_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
+    /* bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
+    Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -31,4 +42,37 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = CabinCondensed,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = CabinCondensed,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    )
 )
+
+
+
+
+
+
+
+
+
+
+
